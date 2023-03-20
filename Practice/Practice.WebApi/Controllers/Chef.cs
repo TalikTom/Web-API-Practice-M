81cb152a5d5717e6814d0edc59bbf5a23737d2b6
Luka Agic <agic.luke@gmail.com>
2023-03-20 11:02:13 +0100

    Initial commit for Chef Controller; Create Chef Controller, add chef class

---
+++ Practice/Practice.WebApi/Controllers/Chef.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practice.WebApi.Controllers
+{
+    public class Chef
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public Guid Id { get; set; }
+
+        public bool Certified { get; set; }
+    }
+}
