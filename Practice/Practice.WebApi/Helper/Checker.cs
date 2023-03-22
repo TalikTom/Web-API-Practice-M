2feb4dbe0306f5e7dadd17b45483f977923ab288
Luka Agic <agic.luke@gmail.com>
2023-03-22 14:52:16 +0100

    Update chef via PUT method, check before if exists with custom helper method;

---
+++ Practice/Practice.WebApi/Helper/Checker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using System.Web.Razor.Generator;
+using Microsoft.Extensions.Logging;
+using Practice.WebApi.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+using System.Runtime.Remoting.Messaging;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlTypes;
+
+
+namespace Practice.WebApi.Helper
+{
+    public static class Checker
+    {
+       public static bool CheckId(Guid id)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Restaurant"].ConnectionString;
+            
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+
+                    SqlCommand cm = new SqlCommand("select * from chef where Id=@id", connection);
+
+                    cm.Parameters.AddWithValue("@id", id);
+                    
+
+                    connection.Open();
+
+                    SqlDataReader reader = cm.ExecuteReader();
+
+
+                if (reader.HasRows)
+                {
+                    return true;
+                }
+                else
+                    
+                reader.Close();
+
+                return false;
+                   
+                }  
+       
+        }
+
+    }
+}
